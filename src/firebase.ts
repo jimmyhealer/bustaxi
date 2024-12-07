0e550ae29e520df4ee65572b221d1c73745d3811
jimmyhealer <yahing6066@gmail.com>
2024-12-08 01:43:58 +0800

    Chore: add firebase module

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeoAtDpC8a9eDnNVEPWyh4qrXm4sJbSCo",
+  authDomain: "busapp-120d9.firebaseapp.com",
+  projectId: "busapp-120d9",
+  storageBucket: "busapp-120d9.firebasestorage.app",
+  messagingSenderId: "537749523433",
+  appId: "1:537749523433:web:838d187d4a1c3fb79d1978",
+  measurementId: "G-G3S842JDLS"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
